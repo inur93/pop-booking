aae2467cf256e01dae7c18a27b48a1f7bb837e53
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2019-07-23 19:34:53 +0200

    updated corsfilter and other minor changes. now only certain origins are allowed

--- pop-booking-api/src/dk/knet/pop/booking/filters/CorsFilter.java
+++ pop-booking-api/src/dk/knet/pop/booking/filters/CorsFilter.java
@@ -1,5 +1,6 @@
 package dk.knet.pop.booking.filters;
 
+import dk.vormadal.configservice.Configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.Logger;
 
@@ -8,9 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+
+import static dk.vormadal.configservice.Configuration.get;
 
 /**
  * Created: 17-02-2018
@@ -19,32 +28,62 @@ import java.io.IOException;
 @Slf4j
 @Priority(500) //Before AuthorizationFilter (1000) to make sure that headers always get injected
 @Provider
-public class CorsFilter implements ContainerRequestFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
     @Context
     private HttpServletRequest request;
     @Context
     private HttpServletResponse response;
 
-    public CorsFilter(){
+    private final Set<String> allowedOrigins;
+    private final String primaryOrigin;
+
+    public CorsFilter() {
         System.out.println("Cors filter");
+        String allowed = get("booking.cors.allowed_origins", "*");
+        String[] values = allowed.split(";");
+        primaryOrigin = values[0];
+        allowedOrigins = new HashSet<>(Arrays.asList(values));
+
+
         log.error("Cors filter");
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        log.debug("Request caught in CORS-Filter:" +request.getRequestURI());
-        setCORSHeaders();
+        if (isPreflightRequest(requestContext)) {
+            requestContext.abortWith(Response.ok().build());
+        }
     }
 
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
+        if (origin == null) {
+            return;
+        }
 
-    private void setCORSHeaders() {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, PATCH");
-        String requestAllowHeader = request.getHeader("Access-Control-Request-Headers");
-        response.setHeader("Access-Control-Allow-Headers",requestAllowHeader);
-        response.setHeader("Access-Control-Allow-Credentials:", "true");
-        response.setHeader("Access-Control-Expose-Headers","Authorization");
-        response.setHeader("encoding", "utf-8");
+        if (isPreflightRequest(requestContext)) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, PATCH");
+            String requestAllowHeader = request.getHeader("Access-Control-Request-Headers");
+            response.setHeader("Access-Control-Allow-Headers", requestAllowHeader);
+            response.setHeader("Access-Control-Allow-Credentials:", "true");
+            response.setHeader("Access-Control-Expose-Headers", "Authorization");
+            response.setHeader("encoding", "utf-8");
+        }
+
+        response.setHeader("Vary", "Origin"); //Access-Control-Allow-Origin might vary - tell the browser
+        String allowed = primaryOrigin;
+        if (allowedOrigins.contains(origin)) {
+            allowed = origin;
+        } else if (allowedOrigins.contains("*")) { //if everyone is allowed then let them know - should be avoided though
+            allowed = "*";
+        }
+        response.setHeader("Access-Control-Allow-Origin", allowed);
+    }
+
+    private boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null
+                && request.getMethod().equalsIgnoreCase("OPTIONS");
     }
 
 }
